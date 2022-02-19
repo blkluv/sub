cbf623b1a701d9716399506b5a25f60587062f50
Matt <matt@pinata.cloud>
2022-02-19 11:47:54 -0600

    Fixed copy paste import

--- db/models/index.js
+++ db/models/index.js
@@ -7,7 +7,6 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 // const config = require(__dirname + '/../config/config.json')[env];
 const config = require('../config/config');
-const {EnvVars} = require("../../../../pinata/pinata-managed/src/enums");
 const db = {};
 
 let sequelize;

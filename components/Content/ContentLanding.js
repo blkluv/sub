9e007d3dbe0343eecc077dcb362dd5334ef5aae0
polluterofminds <justin.edward.hunter@gmail.com>
2022-02-19 14:39:49 -0600

    signature work

--- components/Content/ContentLanding.js
+++ components/Content/ContentLanding.js
@@ -1,38 +1,47 @@
-import { useMetamask } from "../../hooks/useMetamask"
-import PinnieYellowSvg from "../PinnieYellowSvg";
+import { useMetamask } from "../../hooks/useMetamask";
+import Pinnie from "../Pinnie";
 import SubmarineLogoSvg from "../SubmarineLogoSvg";
 
 export default function ContentLanding({ pageData }) {
   const { signData } = useMetamask();
 
   return (
-    <div className="public-content-bg h-screen w-screen flex flex-col justify-center align-center">
-       <div className="p-10 md:w-1/2 w-3/4 h-auto text-center flex flex-col justify-center align-center m-auto bg-white overflow-hidden shadow-lg rounded-lg">
-        <img className="mb-8 mt-6 w-24 h-24 m-auto rounded-full" src={`https://opengateway.mypinata.cloud/ipfs/${pageData[0].thumbnail}?img-width=200&img-height=200`} alt={`${pageData[0].name} preview`} />
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block">{pageData[0].name}</span>      
-        </h2>
-        <h4 className="mt-4 text-muted text-xl">{pageData[0].description}</h4>        
-        <div className="mt-10 flex justify-center">
-          <div className="inline-flex w-1/2">
-            <button
-              onClick={() => signData(pageData[0].network)}              
-              className="w-full inline-flex shadow-sm items-center justify-center px-5 py-3 text-base font-medium rounded-full text-white bg-pinata-purple hover:bg-pinata-purple"
-            >
-              Connect wallet
-            </button>
-          </div>             
-        </div>
-        <p className="mt-4 mb-4 text-md text-muted">Unlock this content by connecting your wallet to verify you have the required NFT.</p>      
-       </div>
-       <div className="p-4 flex flex-row">
-         <div>
+    <div>
+      <div className="absolute p-4 flex flex-row">
+        <div>
           <SubmarineLogoSvg />
-         </div>        
-         <div className="ml-8 -mt-2">
-          <PinnieYellowSvg />
-         </div>        
-       </div>
+        </div>
+      </div>
+      <div className="absolute bottom-10 right-10 z-10">
+          <Pinnie />
+      </div>
+      <div className="public-content-bg h-screen w-screen flex flex-col justify-center align-center">
+        <div className="p-10 md:w-1/2 w-3/4 h-auto text-center flex flex-col justify-center align-center m-auto bg-white overflow-hidden shadow-lg rounded-lg">
+          <img
+            className="mb-8 mt-6 w-24 h-24 m-auto rounded-full"
+            src={`https://opengateway.mypinata.cloud/ipfs/${pageData[0].thumbnail}?img-width=200&img-height=200`}
+            alt={`${pageData[0].name} preview`}
+          />
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <span className="block">{pageData[0].name}</span>
+          </h2>
+          <h4 className="mt-4 text-muted text-xl">{pageData[0].description}</h4>
+          <div className="mt-10 flex justify-center">
+            <div className="inline-flex w-1/2">
+              <button
+                onClick={() => signData(pageData[0].network)}
+                className="w-full inline-flex shadow-sm items-center justify-center px-5 py-3 text-base font-medium rounded-full text-white bg-pinata-purple hover:bg-pinata-purple"
+              >
+                Connect wallet
+              </button>
+            </div>
+          </div>
+          <p className="mt-4 mb-4 text-md text-muted">
+            Unlock this content by connecting your wallet to verify you have the
+            required NFT.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- hooks/useMetamask.js
+++ hooks/useMetamask.js
@@ -6,6 +6,7 @@ export const useMetamask = () => {
   const [ethereum, setEthereum] = useState(null);
   const [url, setUrl] = useState("");
   const [holdsNFT, setHoldsNFT] = useState(false);
+  const [checking, setChecking] = useState(false);
 
   useEffect(() => {
     if (typeof window.ethereum !== "undefined") {
@@ -17,26 +18,98 @@ export const useMetamask = () => {
     }
   }, [ethereum]);
 
+  const signTypedData = async (address, network, id) => {  
+    const networks =  {
+      "ETH - Mainnet": 1, 
+      "ETH - Ropsten": 3, 
+      "ETH - Rinkeby": 4, 
+      "Polygon - Mainnet": 137, 
+      "Polygon - Mumbai": 80001
+    }
+    const from = address;
+
+    const msgParams = JSON.stringify({
+      domain: {        
+        chainId: networks[network],  
+        name: 'Submarine Me',
+        version: '1',
+      },
+  
+      // Defining the message signing data content.
+      message: {
+        contents: `Nonce to sign from server: ${id}`,
+        id: id
+      },
+      // Refers to the keys of the *types* object below.
+      primaryType: 'ProveOwnership',
+      types: {
+        // TODO: Clarify if EIP712Domain refers to the domain the contract is hosted on
+        EIP712Domain: [
+          { name: 'name', type: 'string' },
+          { name: 'version', type: 'string' },
+          { name: 'chainId', type: 'uint256' }
+        ],
+        ProveOwnership: [       
+          { name: "id", type: "string" }, 
+        ]       
+      },
+    });
+
+    // const msgParams = getTypedData(network, message);
+    var params = [from, msgParams];
+    var method = 'eth_signTypedData_v4';
+    setChecking(true);
+    ethereum.sendAsync({
+      method,
+      params,
+      from,
+    }, async function (err, result) {
+      if (err) {
+        setChecking(false);
+        return console.dir(err);
+      } 
+      if (result.error) {
+        setChecking(false);
+        alert(result.error.message);
+      }
+      if (result.error) {
+        setChecking(false);
+        return console.error('ERROR', result)
+      };
+      console.log(result.result);
+      const serverRes = await axios.post("/api/verify", {
+          address: account,
+          params: msgParams,
+          signature
+      });
+    });
+  }
+
   const signData = async (network) => {    
     const messageToSign = await axios.get("/api/verify");
     const accounts = await ethereum.request({ method: "eth_requestAccounts" });
     const account = accounts[0];
-    const typedData = getTypedData(network, messageToSign);    
-    const signature = signTypedData(accounts, network);
-    console.log(signature);
-    try {
-      const res = await axios.post("/api/verify", {
-        address: account,
-        signature
-      });
-      const url = res.data;
+
+    //  HARDCODED TEST
+    network = "ETH - Mainnet"
+
+
+    // const typedData = getTypedData(network, messageToSign);    
+    
+    const signature = await signTypedData(account, network, messageToSign.data.id);
+    // try {
+    //   const res = await axios.post("/api/verify", {
+    //     address: account,
+    //     signature
+    //   });
+    //   const url = res.data;
       
-      setUrl(url);
-    } catch (error) {
-      if (error.response && error.response.status === 401) {
-        setHoldsNFT(false);
-      }
-    }
+    //   setUrl(url);
+    // } catch (error) {
+    //   if (error.response && error.response.status === 401) {
+    //     setHoldsNFT(false);
+    //   }
+    // }
   };
 
   return {

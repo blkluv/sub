bbb7f7a8714b130d1c1710779c8ed9327ae0f6d1
polluterofminds <justin.edward.hunter@gmail.com>
2022-02-25 13:35:22 -0600

    favicon updates

--- pages/submarine/new.js
+++ pages/submarine/new.js
@@ -7,6 +7,22 @@ const SubmarineNew = () => {
   return (
     <div>
       <Head>
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <link rel="icon" href="/submarine.png"></link>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Submarine Me - Unlock Exlusive Content With An NFT"
+        />
+        <meta property="og:type" content="Web application" />
+        <meta property="og:title" content="Submarine Me" />
+        <meta
+          property="og:description"
+          content="Submarine Me - Unlock Exlusive Content With An NFT"
+        />
+        <title>Submarine Me</title>
       <script
           dangerouslySetInnerHTML={{
             __html: `

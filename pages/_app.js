51dc0dac66a0460a8394447fa61e7745c50b0e34
Matias Vallone <matias@pinata.cloud>
2022-09-30 15:27:08 -0300

    adds intercom widget

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "../styles/globals.css";
+import { IntercomProvider } from "react-use-intercom";
+
+const INTERCOM_APP_ID = process.env.NEXT_PUBLIC_INTERCOM_APP_ID || "f4cld255";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <IntercomProvider appId={INTERCOM_APP_ID}>
+      <Component {...pageProps} />;
+    </IntercomProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,26 @@ import Dashboard from "../components/Dashboard";
 import Auth from "../components/Auth";
 import Head from "next/head";
 import SharedHead from "../components/SharedHead";
+import { useIntercom } from "react-use-intercom";
+
 export default function Home() {
-  const { fetchSession, isAuthenticated, plan, confirmMFA, logUserIn } = useAuth();
+  const { fetchSession, isAuthenticated, plan, confirmMFA, logUserIn, loggedInUser } = useAuth();
   useEffect(() => {
     fetchSession();
   }, []);
+
+  const user = loggedInUser?.attributes || {};
+  const { boot, shutdown } = useIntercom();
+
+  useEffect(() => {
+    isAuthenticated
+      ? boot({
+          name: `${user["custom:firstName"]} ${user["custom:lastName"]}`,
+          email: user.email,
+        })
+      : shutdown();
+  }, [isAuthenticated]);
+
   return (
     <div>
       <SharedHead />

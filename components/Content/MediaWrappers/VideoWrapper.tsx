d5d25d06937470c05494ae1eef53f3963a66991c
Paige Jones <pcjones12@gmail.com>
2022-10-18 11:18:10 -0500

    display content within unlock cont, not externally

---
+++ components/Content/MediaWrappers/VideoWrapper.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+const VideoWrapper = ( url) => {
+  console.log({url})
+  return (
+    <ReactPlayer 
+      url={url} 
+      controls={true}
+      playing={true}
+      pip={true}
+      stopOnUnmount={true}
+      className="md:w-3/4 md:h-3/4 w-full h-full"
+      width="100%"
+      height="100%"
+      onError={() => window.open(url, "_blank", "noopener,noreferrer")}
+    />
+  )
+}
+
+export default VideoWrapper
--- components/Content/UnlockedContentContainer.tsx
+++ components/Content/UnlockedContentContainer.tsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAppSelector } from "../../store/hooks";
 import { selectSubmarinedContent } from "../../store/selectors/submarinedContentSelectors";
+import { useState } from "react";
 import Gallery from "./Gallery";
+import VideoWrapper from "./MediaWrappers/VideoWrapper";
+import Image from "next/image";
 
 const UnlockedContentContainer = ({ name }) => {
   const submarinedContent = useAppSelector(selectSubmarinedContent);
+  const [fileType, setFileType] = useState<string>("")
+  const url = `${submarinedContent.gateway}/ipfs/${submarinedContent.cid}?accessToken=${submarinedContent.token}`;
+
+  
+  useEffect(() => {
+    findFileType()
+  }, [fileType]);
+
+  const findFileType = async() => {
+    if(!submarinedContent.directory){
+      const req = await fetch(url, {method:'HEAD'});
+      const content_type = req.headers.get('content-type');
+      if(content_type.includes("image")){
+        setFileType("image")
+      }
+      if(content_type.includes("video")){
+        setFileType("video")
+      }
+      console.log(content_type)
+    }
+  }
+
   if (submarinedContent.directory) {
     return <Gallery name={name} content={submarinedContent} />;
   }
-  const url = `${submarinedContent.gateway}/ipfs/${submarinedContent.cid}?accessToken=${submarinedContent.token}`;
-  window.open(url, "_blank", "noopener,noreferrer");
   return (
     <div>
-      <button onClick={() => window.open(url)}>Content unlocked!</button>
+      {fileType == "image" && <Image src={url} alt="" width={300} height={300}/>}
+      {fileType == "video" && <VideoWrapper url={url}/>}
     </div>
   );
 };

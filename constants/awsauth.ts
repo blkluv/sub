b2de567726d5757726b09a2c3afa81902875d0ad
Matias Vallone <matias@pinata.cloud>
2022-12-02 11:51:04 -0300

    removes cookiestorage for cognito storage

--- constants/awsauth.ts
+++
@@ -1,7 +0,0 @@
-export const awsauth = {
-  domain: process.env.NEXT_PUBLIC_AWS_AUTH_URL,
-  scope: ["email", "profile", "openid", "aws.cognito.signin.user.admin"],
-  redirectSignIn: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}${process.env.NEXT_PUBLIC_LOGIN_REDIRECT}`,
-  redirectSignOut: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}${process.env.NEXT_PUBLIC_SIGNOUT_REDIRECT}`,
-  responseType: "code",
-};

c9562f1136c90dcb388be9372209c7f459c52139
Paige Jones <pcjones12@gmail.com>
2022-12-12 09:18:22 -0600

    pricing card style added

--- components/Billing/Pricing/CardPricing.tsx
+++ components/Billing/Pricing/CardPricing.tsx
@@ -1,6 +1,15 @@
 import styled from "@emotion/styled";
-import { Button, Card, CardContent, ListItem, Tooltip, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  ListItem,
+  Tooltip,
+  Typography,
+  Unstable_Grid2,
+} from "@mui/material";
 import { BillingPlan, Plan } from "../../../store/legacy/billing/types";
+import CheckIcon from "@mui/icons-material/Check";
 
 interface CardPricingProps {
   plan: BillingPlan;
@@ -11,79 +20,85 @@ interface CardPricingProps {
 }
 
 const CardPricing = ({ plan, action, currentPlan, nextPlan, disabled }: CardPricingProps) => {
-  const SelectedPlanCard =
-    currentPlan?.type === plan.type
-      ? styled(Card)`
-          border: 6px solid;
-          border-image-slice: 1;
-          border-image-source: linear-gradient(120deg, var(--purple), orange);
-        `
-      : Card;
   return (
     <ListItem>
-      <SelectedPlanCard
-        sx={{ height: "100%", width: "100%", backgroundColor: disabled ? "#eeeeee" : "white" }}
+      <Card
+        sx={{
+          height: "100%",
+          width: "100%",
+          backgroundColor: currentPlan?.type === plan.type ? "#2a40d7" : "white",
+          color: currentPlan?.type === plan.type ? "white" : "#56DB99",
+          "& .MuiTypography-root": {
+            color: currentPlan?.type === plan.type ? "white" : "black",
+          },
+        }}
       >
         <CardContent sx={{ height: "100%" }}>
-          <div className="d-flex h-100 flex-column align-items-center justify-content-between">
-            <div className="text-center">
+          <Unstable_Grid2 container flexDirection={"column"} gap={"2rem"}>
+            <Unstable_Grid2>
               <Typography variant="h4">{plan.nickname}</Typography>
-              <Typography variant="caption">{plan.subtitle}</Typography>
-              <p className="d-flex flex-column text-muted text-weight-700 text-center mh-25">
-                <span style={{ fontSize: "4em" }}>${plan.price}</span>
-                <span style={{ fontSize: "1em" }}>/month</span>
-              </p>
-              <div className="d-flex flex-column align-items-center">
-                <ul className="font-size-16 list-unstyled text-left">
-                  {plan?.features?.length > 0 &&
-                    plan.features.map((feature, index) => {
-                      return (
-                        <li key={`feature_${index}`} className="mb-1">
-                          <div className="d-flex align-items-center">
-                            <i className="fas fa-check mr-3 text-primary align-self-baseline mt-1" />
-                            <span>{feature}</span>
-                          </div>
-                        </li>
-                      );
-                    })}
-                  {disabled && (
-                    <li style={{ marginTop: "3rem" }}>
-                      <div className="text-center">
-                        <i className="fas fa-check mr-3 text-primary " />
-                        <span>
-                          <br />
-                          <strong>This plan is not valid for Submarine.me</strong>
-                        </span>
-                      </div>
-                    </li>
-                  )}
-                </ul>
-              </div>
-            </div>
-            {currentPlan?.type !== plan.type && nextPlan?.type !== plan.type && (
-              <Button onClick={() => action(plan)} size="large" sx={{ marginTop: 2 }}>
-                Select {plan.nickname}
-              </Button>
-            )}
-            {/* do not allow user click one more time to the already chosen plan */}
-            {nextPlan?.type === plan.type && currentPlan?.type !== plan.type && (
-              <Button disabled={nextPlan?.type === plan.type} size="large" sx={{ marginTop: 2 }}>
-                Your Next Plan
-              </Button>
-            )}
-            {currentPlan?.type === plan.type && nextPlan && nextPlan.type !== plan.type && (
-              <>
-                <Typography variant="caption" sx={{ marginTop: 2 }}>
-                  Your plan will be downgraded
-                </Typography>
-                <Button size="large" onClick={() => action(plan)}>
-                  Keep my current plan
+              <Typography sx={{ fontSize: ".9rem", opacity: ".7" }}>{plan.subtitle}</Typography>
+            </Unstable_Grid2>
+            <Unstable_Grid2>
+              <Typography variant="h2">{plan.type == 0 ? "Free" : "$" + plan.price}</Typography>
+              <Typography> {plan.type == 0 ? "For a lifetime" : "/month"} </Typography>
+              {currentPlan?.type !== plan.type && nextPlan?.type !== plan.type && (
+                <Button onClick={() => action(plan)} sx={{ marginTop: 2, width: "100%" }}>
+                  Select Plan
                 </Button>
-              </>
-            )}
-          </div>
+              )}
+              {currentPlan?.type === plan.type && (
+                <Button
+                  sx={{ backgroundColor: "white", color: "#2a40d7", marginTop: 2, width: "100%" }}
+                >
+                  Current Plan
+                </Button>
+              )}
+            </Unstable_Grid2>
+            <Unstable_Grid2>
+              <ul className="font-size-16 list-unstyled text-left">
+                {plan?.features?.length > 0 &&
+                  plan.features.map((feature, index) => {
+                    return (
+                      <li key={`feature_${index}`} className="mb-1">
+                        <Unstable_Grid2 container gap={"1rem"}>
+                          <CheckIcon />
+                          <Typography sx={{ fontSize: ".8rem" }}>{feature}</Typography>
+                        </Unstable_Grid2>
+                      </li>
+                    );
+                  })}
+                {disabled && (
+                  <li style={{ marginTop: "3rem" }}>
+                    <div className="text-center">
+                      <i className="fas fa-check mr-3 text-primary " />
+                      <Typography variant="body1">
+                        This plan is not valid for Submarine.me
+                      </Typography>
+                    </div>
+                  </li>
+                )}
+              </ul>
+              {/* do not allow user click one more time to the already chosen plan */}
+              {nextPlan?.type === plan.type && currentPlan?.type !== plan.type && (
+                <Button disabled={nextPlan?.type === plan.type} size="large" sx={{ marginTop: 2 }}>
+                  Your Next Plan
+                </Button>
+              )}
+              {currentPlan?.type === plan.type && nextPlan && nextPlan.type !== plan.type && (
+                <>
+                  <Typography variant="caption" sx={{ marginTop: 2 }}>
+                    Your plan will be downgraded
+                  </Typography>
+                  <Button size="large" onClick={() => action(plan)}>
+                    Keep my current plan
+                  </Button>
+                </>
+              )}
+            </Unstable_Grid2>
+          </Unstable_Grid2>
         </CardContent>
-      </SelectedPlanCard>
+      </Card>
     </ListItem>
   );
 };

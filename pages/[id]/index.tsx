5384fb5298b7b19a028df78dd7a28628be984adb
Matias Vallone <matias@pinata.cloud>
2023-01-04 12:00:07 -0300

    removes getServerSideProps, frontend makes API call now

--- pages/[id]/index.tsx
+++ pages/[id]/index.tsx
@@ -1,12 +1,33 @@
 import { Box } from "@mui/system";
 import ky from "ky";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import MainLandingContent from "../../components/Content/MainLandingContent";
+import Loading from "../../components/Dashboard/Loading";
 import PublicLayout from "../../components/Layout/PublicLayout";
 import { getContentReturnObject } from "../api/content/[shortId]";
 
-const Content = ({ data }: { data: getContentReturnObject & { error: any } }) => {
-  const gatewayUrl = `https://${data.gatewayUrl}.${process.env.NEXT_PUBLIC_GATEWAY_ROOT}.cloud`;
+const Content = () => {
+  const [data, setData] = useState<(getContentReturnObject & { error: any }) | null>();
 
+  const gatewayUrl =
+    data && `https://${data.gatewayUrl}.${process.env.NEXT_PUBLIC_GATEWAY_ROOT}.cloud`;
+  const router = useRouter();
+  const { id } = router.query;
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    ky(`/api/content/${id}`, {
+      method: "GET",
+    }).then(async (data) => {
+      const body = await data.json();
+      setData(body);
+    });
+  }, [id]);
+  if (!data) {
+    return <Loading />;
+  }
   return (
     <PublicLayout fileInfo={data}>
       <Box sx={{ minHeight: "100vh", width: "100vw", display: "flex" }}>
@@ -15,23 +36,5 @@ const Content = ({ data }: { data: getContentReturnObject & { error: any } }) =>
     </PublicLayout>
   );
 };
-
-export async function getServerSideProps(context) {
-  try {
-    const host = process.env.NEXT_PUBLIC_VERCEL_URL;
-    const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
-    const now = Date.now();
-    const res = await ky(`${protocol}://${host}/api/content/${context.query.id}`, {
-      method: "GET",
-    });
-    const data: getContentReturnObject = await res.json();
-    process.env.NEXT_PUBLIC_DEBUG &&
-      console.log(Date.now() - now, " --> Time spent fetching content from DB (ms)");
-    return { props: { data } };
-  } catch (error) {
-    console.log(error);
-    return { props: { data: { error: true } } };
-  }
-}
 
 export default Content;

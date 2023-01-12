9505298bde8bb0197a7345150c8035052ebe44b8
Paige Jones <pcjones12@gmail.com>
2023-01-12 10:28:05 -0600

    pdf preview added on unlock screen

--- components/Content/MediaWrappers/DownloadFile.tsx
+++ components/Content/MediaWrappers/DownloadFile.tsx
@@ -1,19 +1,48 @@
-import { Button, Unstable_Grid2 } from "@mui/material";
+import { Button, Unstable_Grid2, Pagination, Box } from "@mui/material";
+import { Document, Page, pdfjs } from "react-pdf";
+import { useState, useEffect } from "react";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import { Download } from "@mui/icons-material";
 
 const DownloadFile = ({ url }: { url: string }) => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useEffect(() => {
+    pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  });
+
+  const handleChange = (event, value) => {
+    setPageNumber(value);
+  };
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
   return (
     <Unstable_Grid2
       container
       justifyContent={"center"}
       alignItems={"center"}
-      sx={{ height: "50%" }}
+      direction={"column"}
+      gap={"1em"}
     >
+      <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} renderAnnotationLayer={false} />
+      </Document>
+      <Pagination size="small" count={numPages} page={pageNumber} onChange={handleChange} />
       <Button
+        href={url}
+        variant="outlined"
         sx={{
-          a: { color: "#fff", textDecoration: "none" },
+          backgroundColor: "white",
+          borderColor: "black",
+          color: "black",
+          marginTop: "1rem",
         }}
       >
-        <a href={url}>Content unlocked! Click here to download</a>
+        <Download />
+        Download
       </Button>
     </Unstable_Grid2>
   );

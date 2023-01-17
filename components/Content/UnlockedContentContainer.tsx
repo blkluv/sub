2b1ac357025c91c4c8f2fcf826c787e664bd6f87
Paige Jones <pcjones12@gmail.com>
2023-01-17 16:00:02 -0600

    added twitch sub link

--- components/Content/UnlockedContentContainer.tsx
+++ components/Content/UnlockedContentContainer.tsx
@@ -2,7 +2,6 @@ import { useAppSelector } from "../../store/hooks";
 import { selectSubmarinedContent } from "../../store/selectors/submarinedContentSelectors";
 import Gallery from "./Gallery";
 import SingleMediaDisplay from "./SingleMediaDisplay";
-import { Box, Unstable_Grid2 } from "@mui/material";
 
 const UnlockedContentContainer = ({ name }) => {
   const submarinedContent = useAppSelector(selectSubmarinedContent);
